7fd1caf1a7bf27d0caff11d8d7653491ff3fbd19
C-INFT1206 <cordell_ct@outlook.com>
2026-08-03 23:37:05 -0400

    Update main.js 2

--- Java Assignment 4/Part 1/main.js
+++ Java Assignment 4/Part 1/main.js
@@ -1,6 +1,18 @@
 /*
-Name: C-INFT
+Name: YOUR NAME
 File: main.js
 Date: 03 August 2026
 JavaScript for generating randomized silly stories.
 */
+
+const customName = document.getElementById("custom-name");
+const generateBtn = document.querySelector(".generate");
+const story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+const random = Math.floor(Math.random() * array.length);
+return array[random];
+}
+
+
+
